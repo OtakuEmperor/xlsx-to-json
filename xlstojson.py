3fd66a48cdf21dfb47277f7884ca6641ec5a9c73
WenYoo <yanrenhuang569@gmail.com>
2020-12-08 00:54:35 +0800

    修改檔案讀取限制

--- xlstojson.py
+++ xlstojson.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python2.7
 import xlrd
 import xlwt
 import json
@@ -6,65 +5,67 @@ import os.path
 import datetime
 
 def getColNames(sheet):
-	rowSize = sheet.row_len(0)
-	colValues = sheet.row_values(0, 0, rowSize )
-	columnNames = []
+    rowSize = sheet.row_len(0)
+    colValues = sheet.row_values(0, 0, rowSize )
+    columnNames = []
 
-	for value in colValues:
-		columnNames.append(value)
+    for value in colValues:
+        columnNames.append(value)
 
-	return columnNames
+    return columnNames
 
 def getRowData(row, columnNames):
-	rowData = {}
-	counter = 0
+    rowData = {}
+    counter = 0
 
-	for cell in row:
-		# check if it is of date type print in iso format
-		if cell.ctype==xlrd.XL_CELL_DATE:
-			rowData[columnNames[counter].lower().replace(' ', '_')] = datetime.datetime(*xlrd.xldate_as_tuple(cell.value,0)).isoformat()
-		else:
-			rowData[columnNames[counter].lower().replace(' ', '_')] = cell.value
-		counter +=1
+    for cell in row:
+        # check if it is of date type print in iso format
+        if cell.ctype==xlrd.XL_CELL_DATE:
+            rowData[columnNames[counter].lower().replace(' ', '_')] = datetime.datetime(*xlrd.xldate_as_tuple(cell.value,0)).isoformat()
+        else:
+            rowData[columnNames[counter].lower().replace(' ', '_')] = cell.value
+        counter +=1
 
-	return rowData
+    return rowData
 
 def getSheetData(sheet, columnNames):
-	nRows = sheet.nrows
-	sheetData = []
-	counter = 1
+    nRows = sheet.nrows
+    sheetData = []
+    counter = 1
 
-	for idx in range(1, nRows):
-		row = sheet.row(idx)
-		rowData = getRowData(row, columnNames)
-		sheetData.append(rowData)
+    for idx in range(1, nRows):
+        row = sheet.row(idx)
+        rowData = getRowData(row, columnNames)
+        sheetData.append(rowData)
 
-	return sheetData
+    return sheetData
 
 def getWorkBookData(workbook):
-	nsheets = workbook.nsheets
-	counter = 0
-	workbookdata = {}
+    nsheets = workbook.nsheets
+    counter = 0
+    workbookdata = {}
 
-	for idx in range(0, nsheets):
-		worksheet = workbook.sheet_by_index(idx)
-		columnNames = getColNames(worksheet)
-		sheetdata = getSheetData(worksheet, columnNames)
-		workbookdata[worksheet.name.lower().replace(' ', '_')] = sheetdata
+    for idx in range(0, nsheets):
+        worksheet = workbook.sheet_by_index(idx)
+        columnNames = getColNames(worksheet)
+        sheetdata = getSheetData(worksheet, columnNames)
+        workbookdata[worksheet.name.lower().replace(' ', '_')] = sheetdata
 
-	return workbookdata
+    return workbookdata
 
 def main():
-	filename = input("Enter the path to the filename -> ")
-	if os.path.isfile(filename):
-		workbook = xlrd.open_workbook(filename)
-		workbookdata = getWorkBookData(workbook)
-		output = \
-		open((filename.replace("xlsx", "json")).replace("xls", "json"), "w+")
-		output.write(json.dumps(workbookdata, sort_keys=True, indent=2,  separators=(',', ": ")))
-		output.close()
-		print ("%s was created" %output.name)
-	else:
-		print ("Sorry, that was not a valid filename")
-
-main()
+    filename = input("Enter the path to the filename -> ")
+    if os.path.isfile(filename):
+        workbook = xlrd.open_workbook(filename)
+        workbookdata = getWorkBookData(workbook)
+        output = \
+        open((filename.replace("xlsx", "json")).replace("xls", "json"), "w+")
+        output.write(json.dumps(workbookdata, sort_keys=True, indent=2,  separators=(',', ": ")))
+        output.close()
+        print ("%s was created" %output.name)
+    else:
+        print ("Sorry, that was not a valid filename")
+allfile = os.listdir(os.getcwd())
+for excel in allfile:
+    if os.path.isfile(excel) and excel.endswith(".xlsx"):
+        main(excel)
